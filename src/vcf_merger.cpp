19d24480d8d3b2f8c1b895fc735f121e0783cdbd
Davide Bolognini <davide.bolognini@hm-m7qgmwvvpw.htechnopole.local>
2026-03-02 14:56:20 +0100

    restore partial logging

--- src/vcf_merger.cpp
+++ src/vcf_merger.cpp
@@ -162,8 +162,8 @@ bool VCFMerger::mergeChunks(const std::vector<std::string>& chunk_files) {
     
     for (size_t chunk_idx = 0; chunk_idx < chunk_files.size(); ++chunk_idx) {
         const auto& chunk_file = chunk_files[chunk_idx];
-        logDebug("Processing chunk " + std::to_string(chunk_idx + 1) + "/" + 
-                std::to_string(chunk_files.size()) + ": " + chunk_file);
+        logInfo("Processing chunk " + std::to_string(chunk_idx + 1) + "/" + 
+               std::to_string(chunk_files.size()) + ": " + chunk_file);
         
         BGZF* in_fp = nullptr;
         std::ifstream in_file;
